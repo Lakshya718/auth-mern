62bfe01f5e4a7468d53cab86c99cb6b1e7488fa9
Lakshya_059 <lakshyakantiwal@gmail.com>
2024-08-04 02:01:28 +0530

    add redux toolkit

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -19,14 +19,14 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
-    if (!user) return next(errorHandler(404, "User not found"));
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
 
-    const isValidPassword = bcryptjs.compareSync(password, user.password);
-    if (!isValidPassword) return next(401, "Invalid Credentials");
+    const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!isValidPassword) return next(errorHandler(401, "Wrong Credentials"));
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    const { password: hashedPassword, ...rest } = user._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000);
     res 
       .cookie("access_token", token, { httpOnly: true,expires:expiryDate })
