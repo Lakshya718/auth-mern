57ebaf0c6da6f323efeb26552d5ce0a802aba8db
Lakshya_059 <lakshyakantiwal@gmail.com>
2024-08-03 11:23:38 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose, { connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 app.listen(3000, () => {
-  console.log("Server is listening on the port 3000!...");
+  console.log("Server is listening on the port 3000");
 });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Profile from "./pages/Profile.jsx";
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+     <BrowserRouter>
+       <Header/>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/signin" element={<SignIn />} />
+         <Route path="/signup" element={<SignUp />} />
+         <Route path="/profile" element={<Profile />} />
+       </Routes>
+     </BrowserRouter>
   );
 }
