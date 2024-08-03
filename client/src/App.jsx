af3b4ac5f9717791fb45a11aebe984d3f6e1fe73
Lakshya_059 <lakshyakantiwal@gmail.com>
2024-08-03 11:45:06 +0530

    create user model

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,15 @@ import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
 export default function App() {
   return (
-     <BrowserRouter>
-       <Header/>
-       <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/about" element={<About />} />
-         <Route path="/signin" element={<SignIn />} />
-         <Route path="/signup" element={<SignUp />} />
-         <Route path="/profile" element={<Profile />} />
-       </Routes>
-     </BrowserRouter>
+    <BrowserRouter>
+      <Header />  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
